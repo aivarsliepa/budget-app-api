f831e75d8a5ba4037f373a887596fa68bf481a53
Aivars Liepa <aivarsliepa1992@gmail.com>
2019-02-06 12:32:41 +0200

    Initial commit | Spring Initializr

---
+++ src/main/java/com/aivarsliepa/budgetappapi/BudgetappapiApplication.java
@@ -0,0 +1,14 @@
+package com.aivarsliepa.budgetappapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetappapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BudgetappapiApplication.class, args);
+    }
+
+}
+
