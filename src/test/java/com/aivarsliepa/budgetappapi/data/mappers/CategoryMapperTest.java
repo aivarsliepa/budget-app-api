74f5a421b64161356521ee673ef4b0470f450001
Aivars Liepa <aivarsliepa1992@gmail.com>
2019-02-22 11:47:15 +0200

    reducing duplicate code

--- src/test/java/com/aivarsliepa/budgetappapi/data/mappers/CategoryMapperTest.java
+++ src/test/java/com/aivarsliepa/budgetappapi/data/mappers/CategoryMapperTest.java
@@ -3,6 +3,7 @@ package com.aivarsliepa.budgetappapi.data.mappers;
 import com.aivarsliepa.budgetappapi.data.dto.CategoryData;
 import com.aivarsliepa.budgetappapi.data.enums.CategoryType;
 import com.aivarsliepa.budgetappapi.data.models.CategoryModel;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -14,45 +15,39 @@ public class CategoryMapperTest {
 
     private CategoryMapper mapper = new CategoryMapper();
 
-    @Test
-    public void shouldMapDataToModel() {
+    private CategoryData data;
+    private CategoryModel model;
+
+    @Before
+    public void beforeEach() {
         var type = CategoryType.EXPENSE;
         var parentId = 2L;
         var id = 1L;
 
-        var data = new CategoryData();
+        data = new CategoryData();
         data.setId(id);
         data.setParentId(parentId);
         data.setCategoryType(type);
 
-        var expected = new CategoryModel();
-        expected.setCategoryType(type);
-        expected.setParentId(parentId);
-        expected.setId(id);
+        model = new CategoryModel();
+        model.setCategoryType(type);
+        model.setParentId(parentId);
+        model.setId(id);
+    }
+
+    @Test
+    public void shouldMapDataToModel() {
 
         var actual = mapper.map(data);
 
-        assertEquals(expected, actual);
+        assertEquals(model, actual);
     }
 
     @Test
     public void shouldMapModelToData() {
-        var type = CategoryType.INCOME;
-        var parentId = 1L;
-        var id = 2L;
-
-        var model = new CategoryModel();
-        model.setCategoryType(type);
-        model.setParentId(parentId);
-        model.setId(id);
-
-        var expected = new CategoryData();
-        expected.setId(id);
-        expected.setParentId(parentId);
-        expected.setCategoryType(type);
 
         var actual = mapper.map(model);
 
-        assertEquals(expected, actual);
+        assertEquals(data, actual);
     }
 }
