02a99884495d0a79508f468464ec22916ec02498
Aivars Liepa <aivarsliepa1992@gmail.com>
2019-02-28 13:16:30 +0200

    unsupported method handler and debugging

--- src/main/java/com/aivarsliepa/budgetappapi/exceptionhandlers/MvcExceptionHandlers.java
+++ src/main/java/com/aivarsliepa/budgetappapi/exceptionhandlers/MvcExceptionHandlers.java
@@ -1,28 +1,39 @@
 package com.aivarsliepa.budgetappapi.exceptionhandlers;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+@Slf4j
 @ControllerAdvice
 public class MvcExceptionHandlers {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public void handleMethodArgumentNotValidException() {
+    public void handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        log.debug(ex.getMessage());
     }
 
 
     @ExceptionHandler(HttpMessageNotReadableException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public void handleHttpMessageNotReadableException() {
+    public void handleHttpMessageNotReadableException(HttpMessageNotReadableException ex) {
+        log.debug(ex.getMessage());
     }
 
     @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public void handleGlobalException() {
+    public void handleGlobalException(Exception ex) {
+        log.debug(ex.getMessage());
+    }
+
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    @ResponseStatus(HttpStatus.METHOD_NOT_ALLOWED)
+    public void handleHttpRequestMethodNotSupportedException() {
     }
 }
